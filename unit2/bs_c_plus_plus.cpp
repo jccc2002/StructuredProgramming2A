fcfe70d89693e32f7c56a08b8b4f5c288fe89aad
Jorge Canul <jccc02.2002@gmail.com>
2021-05-24 11:43:51 -0500

    Error solved

--- unit2/bs_c_plus_plus.cpp
+++ unit2/bs_c_plus_plus.cpp
@@ -3,7 +3,7 @@
 
 int main(){
         //printf("Hello world in C++")
-        std::cout<<"Hello"<<"world"<<"in C++"
+        std::cout<<"Hello"<<"world"<<"in C++";
         return EXIT_SUCCESS;
 
 }
