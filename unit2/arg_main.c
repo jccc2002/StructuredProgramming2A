b906decc6685895aa52abdc480d7d386f8c754de
Jorge Canul <jccc02.2002@gmail.com>
2021-06-01 13:40:15 -0500

    bucles.py done

--- unit2/arg_main.c
+++ unit2/arg_main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
 
+
+int my_int = 0; //%i o %d
+float my_float = 3.14; // %f
+char my_char = 'c'; //%c
+char my_str[10] = "hola"; //%s
+
+
+
+float second_float = 0.0;
+
+//Pointer//
+int * my_prt_int = &my_int; //%p
+
+
+
 int main(int argc, char** argv){
 
-    printf("argc: %i, element 1: %s, elem2: %s char. %c \n", argc, argv[1], argv[2], 'J');
+    strcpy(my_str, argv[1]);
+    int num = atoi(argv[1]);
+    printf("num: %i", num)
+
+    printf("myint: %i , my_float: %f , my_char: %c , my_str: %s , my_ptr_int: %p.\n", my_int, my_float, my_char, my_str, my_prt_int);
+    printf("argc: %i, element 1: %s, elem2: %s char. %c \n", num, argv[2], 'J');
 
     return 0;
 }
---
+++ unit2/bucles.py
@@ -0,0 +1,31 @@
+import sys
+
+
+lista = ["red", "black", "white", "gray", "green"]
+
+#List Comprehension
+lista2 = [i for i in range(1,101) if i%2==0]
+
+listaEvenOdd = [i for i in range(1,101) if i%2==0],[i for i in range(1,101) if i%2!=0]
+
+print(listaEvenOdd)
+
+if __name__ == "__main__":
+
+    print(f' programName: { sys.argv[0]}\n')
+    print(lista, len(lista)) #Size of a list
+    #Manual
+    # print(lista[0])
+    # print(lista[1])
+    # print(lista[2])
+    ##...
+    ##...
+    ##...
+    index = 0
+    while(index < 4):
+        print(f'index:{index}, value: {lista[index]}')
+        index = index + 1
+    print("--------")
+
+    for index in range(0, len(lista)):
+        print(f'index:{index}, value: {lista[index]}')
