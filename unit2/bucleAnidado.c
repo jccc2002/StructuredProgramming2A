e1497334e06d1e499997ddf9d3f8a8db5a51df8a
Jorge Canul <jccc02.2002@gmail.com>
2021-06-07 11:11:00 -0500

    bucleAnidado Done

---
+++ unit2/bucleAnidado.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> //atoi, atof
+#include <stdbool.h>
+
+
+
+#define TAN 10
+
+int lista[TAN] = {105,20,21,40,33,60,70,50,93,100};
+int idx = 0;
+
+typedef int INTEGER;
+
+int main(int argc, char** argv){
+    int index2 = 0;
+    for(int index = 1; index < TAN; index++, index2++){
+
+        printf("index: %d, value: %d. \n\a", index, lista[index]);
+        for (size_t i = 0; i < 3; i++){
+            printf("Holamundo!\n");
+        }
+     }
+    return 0;
+}
--- unit2/bucles.c
+++ unit2/bucles.c
@@ -9,20 +9,35 @@
 int lista[TAN] = {10,20,30,40,50,60,70,80,90,100};
 int idx = 0;
 
+typedef struct nodePr{
+    int size;
+    struct node{
+        char * str;
+    };
+
+}LIST;
+
+LIST myStruct = {1, {"hola"}};
+
+typedef int INTEGER;
+
 int main(int argc, char** argv){
 
     int index2 = 0;
     for(int index = 1; index <= TAN; index++, index2++){
 
         printf("index: %d, value: %d. \n\a", index, lista[index]);
+        for (INTEGER i = 0; i < 4; i++){
+
+        }
     }
 
     while (true)
     {
         if(lista[idx == 50]){
-            break
+            break;
         }
-        idx++
+        idx++;
     }
     printf("idx found: %d", idx);
 
