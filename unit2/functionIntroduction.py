ba3aea1567eddf313bfa5e83d47b594a90600bf1
Jorge Canul <jccc02.2002@gmail.com>
2021-06-08 13:42:33 -0500

    Exercises undone

--- unit2/functionIntroduction.py
+++ unit2/functionIntroduction.py
@@ -2,8 +2,29 @@ from sys import argv as ag
 
 
 def addTwoNumbers(number1, number2):
-    return number1 + number2
+    print("Program already executed")
+    result = number1 + number2
+    return result
 
+
+answer = False
+
+def isEven (aNumber):
+    if(aNumber%2 == 0):
+        return True
+        #print("It is even")
+    else:
+        return False
+        #print("It is an odd")
 
 if __name__ == "__main__":
-    print(f'La suma de dos números = {addTwoNumbers(int(ag[1]), int(ag[2]))}')
+    #print(f'La suma de dos números = {addTwoNumbers(int(ag[1]), int(ag[2]))}')
+    n1 =int(input('Número 1:\t'))
+    n2 =int(input('Número 2:\t'))
+
+    #print(f'La suma de dos números = { addTwoNumbers(n1,n2)}')
+
+    if(isEven (addTwoNumbers(n1,n2))):
+        print(f'N1: "{n1}" and N2:"{n2}" are lucky numbers')    
+    else:
+        print(f'N1: "{n1}" and N2:"{n2}" arent lucky numbers')
