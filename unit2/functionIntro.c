8f847f6ea1d6ce4d99406079ba0f7bc81b22b385
Jorge Canul <jccc02.2002@gmail.com>
2021-06-14 10:35:04 -0500

    working with fuctionts

--- unit2/functionIntro.c
+++ unit2/functionIntro.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
+#include "./headerPractice.h"
 
 int myNumber = 3;
 float myFloat = 3.1416;
 char myString[255] = "Hola mundo";
 
 //Declarar y definirla
-void sayHello(char message[]){
-    printf("%s\n", message);
+//void sayHello(char message[]){
+   // printf("%s\n", message);
 
-}
+//}
 
 //Segundo método para funciones
 
 //1. Declarar
 
-void sayMyname(char message []);
+//void sayMyname(char message []);
 
-
+//int addTwoNumbers(int argument1,int argument2); //Declarando;
 
 int main( ){
-
+    
     sayHello("Hello world");
     sayMyname("Jorge");
+    int myResult = addTwoNumbers(2,5);
+    printf("myResult is equal to: %d \n", myResult);
+
+    printf("%d", libInteger);
     return 0;
 }
 
 //2 Definir
-void sayMyname(char message[]){
-    printf("%s\n", message);
-}
+//void sayMyname(char message[]){
+//    printf("%s\n", message);
+//}
+
+//int addTwoNumbers(int argument1,int argument2){
+  //  int result = argument1 + argument2;
+    //return result; 
+//}
---
+++ unit2/headerPractice.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void sayHello(char message[]){
+    printf("%s\n", message);
+}
+
+void sayMyname(char message[]){
+    printf("%s\n", message);
+}
+
+int addTwoNumbers(int argument1,int argument2){
+    int result = argument1 + argument2;
+    return result; 
+}
