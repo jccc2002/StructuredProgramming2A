3f4afa96c464ea312d5142dd3d2c4d4e088150d0
Jorge Canul <jccc02.2002@gmail.com>
2021-07-06 13:45:06 -0500

    calloc, malloc, realloc, and free done

--- unit2/callback.c
+++ unit2/callback.c
@@ -11,21 +11,28 @@ void callTonumber(int number){
     //Logic
     printf("Calling %d...\n", number);
 }
-void Securitycamera(void (*callTonumber)(int number), int EmergencyNumber);
+
+void sendMessageNumber(int number){
+    printf("sending message to %d \n...",number);
+}
+
+void Securitycamera(void (*cb)(int number), int EmergencyNumber);
 
 int main(){
     // for(;;){
         Securitycamera(callTonumber,911);
+        Securitycamera(sendMessageNumber,911);
     
 
 
     return 0;
 }
 
-void Securitycamera(void (*callTonumber)(int number), int EmergencyNumber){
+void Securitycamera(void (*cb)(int number), int EmergencyNumber){
     //Lógica compleja// 
-    bool thief = true;
+    bool thief = false;
     if (thief){
-        callTonumber(EmergencyNumber);
+        cb(EmergencyNumber);
     } //Si detecta ladrón, debe hacer algo
 }
+
