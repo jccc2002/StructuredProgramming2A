d0ec5eb314330da2da98fcc29d8cdd04e0a7d361
Jorge Canul <jccc02.2002@gmail.com>
2021-07-23 17:05:35 -0500

    project done

--- unit2/Project_U2/D2_for.py
+++ unit2/Project_U2/D2_for.py
@@ -9,3 +9,5 @@ if __name__ == "__main__":
     number6 = [n - 12 for n in range(17,18)]    
     number7 = [n - 13 for n in range(19,22)]
     print(number + number2 + number3 + number4 + number5 + number6 + number7)
+
+    #I didn't find another way to do the exercise. I'm so sorry teacher ):
